280eb070861954078b320f559470399d8543cda0
kwontaeyeon0310 <kwontaeyeon2009@gmail.com>
2025-06-04 10:05:29 +0900

    js

---
+++ js/wev21(js).js
@@ -0,0 +1,29 @@
+let heading = document.getElementById('heading');
+        heading.onclick = function() {
+            heading.style.color = 'red';
+}
+// alert("안녕하세요");
+
+// confirem("안녕하세요");
+
+// let u_name = prompt("이름이 뭔가요");
+// document.writeln("<h1>" + u_name + " 반 갑</h1>");
+
+// console.log("<h1" + u_name + "반갑<h1></h1>") 
+
+// let width;
+// let height;
+
+// width = 200;
+// height =  50;
+// let area = width * height;
+// console.log(area)
+
+// const PI = 3.14;
+// let radius = prompt("반지름을 입력하세요.");
+// let area = PI * radius * radius;
+// console.log(area) 
+
+console.log("I'm a student");
+
+d
